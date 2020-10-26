5cfaec9215a68c2f5953c66e91cb9d4db1422b24
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-10-26 11:49:36 -0400

    Add the redirect from login page to home page at the end of the scripts and remove all other occurrences

--- login.php
+++ login.php
@@ -11,19 +11,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (doCredentialsExist($username, $password)) {
             $_SESSION["loggedin"] = true;
             $_SESSION["username"] = $username;
-            header("location: home.php");
         }
         else {
             $_SESSION["errormsg"] = "Invalid login credentials";
-            header("location: home.php");
         }
     }
     catch (Exception $e) {
         $_SESSION["errormsg"] = $e->getMessage();
-        header("location: home.php");
     }
 }
 else {
     $_SESSION["errormsg"] = "Invalid HTTP method";
-    header("location: home.php");
 }
+header("location: home.php");
