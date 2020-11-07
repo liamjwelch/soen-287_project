ddd91f7ef3257bf6c07ea2b76c843aa3907dd806
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-11-07 11:35:07 -0500

    Use loopback address as 'localhost' does not work on mac

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $table_name = "users";
 
 function createConnection() {
-    $conn = new mysqli("localhost",get_current_user(), "password", "soen287");
+    $conn = new mysqli("127.0.0.1",get_current_user(), "password", "soen287");
 
     if ($conn->connect_errno) {
         throw new Exception("Failed to connect to MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error);
