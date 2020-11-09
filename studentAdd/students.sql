bba3d81be8beca985a1108e6dfb7801ebf575410
liamjwelch <39309947+liamjwelch@users.noreply.github.com>
2020-11-09 16:18:25 -0500

    New column for a unique username

--- studentAdd/students.sql
+++ studentAdd/students.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 04, 2020 at 11:46 PM
+-- Generation Time: Nov 09, 2020 at 10:17 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -29,22 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `students` (
   `student_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `first_name` char(20) NOT NULL,
   `last_name` char(20) NOT NULL,
   `phone_number` int(10) NOT NULL,
   `email_address` char(75) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`student_id`, `first_name`, `last_name`, `phone_number`, `email_address`) VALUES
-(7, 'Liam', 'Welch', 234243, 'liamjwelch@gmail.com'),
-(10, 'check', '123', 6767676, 'check2ndcheck'),
-(12, 'check', 'this is another test', 234234, 'asdfasdfasdf'),
-(14, 'asdfasdf', 'dafsdfasdf', 45345, 'yes@gmail.com'),
-(15, 'This is a final test', 'for today', 519504464, 'finaltest@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +44,8 @@ INSERT INTO `students` (`student_id`, `first_name`, `last_name`, `phone_number`,
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`student_id`);
+  ADD PRIMARY KEY (`student_id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -64,7 +55,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
