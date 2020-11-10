699ca9da6b6ebafadab1466a521c6706db48bba7
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-11-10 10:28:12 -0500

    Add html form on the login page

--- login.php
+++ login.php
@@ -11,6 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (doCredentialsExist($username, $password)) {
             $_SESSION["loggedin"] = true;
             $_SESSION["username"] = $username;
+            header("location: home.php");
         }
         else {
             $_SESSION["errormsg"] = "Invalid login credentials";
@@ -23,7 +24,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION["errormsg"] = $e->getMessage();
     }
 }
-else {
-    $_SESSION["errormsg"] = "Invalid HTTP method";
-}
-header("location: home.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Login - AHED</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+<main class="form">
+	<form class="login-form" method="post" action="login.php">
+		<input type="text" name="username" placeholder="Username"/>
+		<input type="password" name="password" placeholder="Password"/>
+		<button type="submit" name="login">Login</button>
+		<p class="message">Not registered?
+			<a href="register.php">Create an account.</a>
+		</p>
+		<p class="error-message">
+        </p>
+	</form>
+</main>
+</body>
+</html>
