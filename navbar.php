8e0bc4dd7ed435576d717219ad6000fd86037af9
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-11-11 06:04:01 -0500

    Home link in navbar use the new homepage

--- navbar.php
+++ navbar.php
@@ -3,5 +3,5 @@
     <a href="profile.html"><i class="fa fa-fw fa-user fa-lg icons"></i> Profile</a>
     <a href="search.html"><i class="fa fa-fw fa-search fa-lg icons"></i> Search</a>
     <a href="recommendations.html"><i class="fa fa-fw fa-graduation-cap fa-lg icons"></i> Recommendations</a>
-    <a href="home.html"><i class="fa fa-fw fa-home fa-lg icons"></i> Home</a>
+    <a href="homepage.php"><i class="fa fa-fw fa-home fa-lg icons"></i> Home</a>
 </nav>
