b4e66f6ab0aa484cf0d536b39eb087867579be79
esolis14 <e_solis@encs.concordia.ca>
2020-11-11 02:44:14 +0100

    homepage and its css added

---
+++ homepage.php
@@ -0,0 +1,83 @@
+<?php
+$title = "North America Higher Education Database";
+$styles = ["css/homepage.css"];
+$content = '
+    
+    <section class="welcome-msg">
+        <p>WELCOME BACK USER!</p>
+    </section>
+    
+    <!-- AUTOMATIC SLIDESHOW IMAGES -->
+    <article class="slideshow">
+        <img src="images/mit.jpg" alt="MIT" class="slides_image">
+        <img src="images/harvard.jpg" alt="Harvard" class="slides_image">
+        <img src="images/stanford.jpg" alt="Stanford" class="slides_image">
+        <img src="images/mcgill.jpg" alt="McGill" class="slides_image">
+        <img src="images/concordia.jpg" alt="Concordia" class="slides_image">
+        <img src="images/toronto.jpg" alt="Toronto" class="slides_image">
+    </article>
+    
+    <section class="page-sec sec1">
+        <h1>AMERICAN HIGHER EDUCATION DATABASE</h1>
+        <p>OUR INFO OUR INFO OUR INFO OUR INFO</p>
+        <p>OUR INFO OUR INFO OUR INFO OUR INFO</p>
+        <p>OUR INFO OUR INFO OUR INFO OUR INFO</p>
+        <p>OUR INFO OUR INFO OUR INFO OUR INFO</p>
+    </section>
+    
+     <section class="page-sec sec2">
+        <h1>OUR SERVICES</h1>
+        <article class="services">
+            <section>
+                <img src="images/star.ico" class="icons">
+                <h2>RECOMMENDATIONS</h2>
+                <p>Recommendation basic info</p>
+            </section>
+            <section>
+                <img src="images/users.ico" class="icons">
+                <h2>PROFILES</h2>
+                <p>Profiles basic info</p>
+            </section>
+            <section>
+                <img src="images/map.ico" class="icons">
+                <h2>LOCATION</h2>
+                <p>Geographical search basic info</p>
+            </section>
+        </article>
+     </section>
+     
+    <section class="page-sec sec3">
+        <div class="card">
+            <h2>INFORMATION</h2>
+            <p>We could place information in cards</p>
+            <p>They are a nice way to display information</p>
+        </div>
+        <div class="card">
+            <h2>INFORMATION</h2>
+            <p>We could place information in cards</p>
+            <p>They are a nice way to display information</p>
+        </div>
+    </section>
+	
+	<script>
+        // Automatic Slideshow - change image every 4 seconds
+        var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("slides_image");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+          }
+          myIndex ++;
+          if (myIndex > x.length) {
+            myIndex = 1
+          }    
+          x[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 4000);    
+        }
+    </script>
+';
+
+include "template.php";
