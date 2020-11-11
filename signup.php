c0ec581c47a904f999da92a29c0305a6b04805de
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-11-11 09:37:49 -0500

    Make all input fields required

--- signup.php
+++ signup.php
@@ -20,24 +20,24 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 	<form class="signup-form" method="post" action="create_user.php">
         <fieldset>
             <legend>Account Information</legend>
-            <label>Your Email Address:<input type="text" name="username" placeholder="me@example.com"/></label>
-            <label>Your Password:<input type="password" name="password" placeholder="Password"/></label>
+            <label>Your Email Address:<input type="text" name="username" placeholder="me@example.com" required></label>
+            <label>Your Password:<input type="password" name="password" placeholder="Password" required></label>
         </fieldset>
         <fieldset>
             <legend>Personal Information</legend>
-            <label>First Name:<input type="text" name="first_name"></label>
-            <label>Last Name:<input type="text" name="last_name"></label>
-            <label>Phone Number:<input type="tel" name="telephone"></label>
-            <label>Home Address:<input type="text" name="address"></label>
+            <label>First Name:<input type="text" name="first_name" required></label>
+            <label>Last Name:<input type="text" name="last_name" required></label>
+            <label>Phone Number:<input type="tel" name="telephone" required></label>
+            <label>Home Address:<input type="text" name="address" required></label>
         </fieldset>
         <fieldset>
             <legend>Academic Information</legend>
             <label>What do you want to study?
                 <select name="programs" id="programs"><option value="computer_science">Computer Science</option></select>
             </label>
-            <label>What is your GPA?<input type="number" min="0" max="4.3"></label>
+            <label>What is your GPA?<input type="number" min="0" max="4.3" required></label>
         </fieldset>
-        <label><input type="checkbox" name="tc_agreed" class="checkbox">I agree with the <a href="terms.php">terms and conditions</a></label>
+        <label><input type="checkbox" name="tc_agreed" class="checkbox" required>I agree with the <a href="terms.php">terms and conditions</a></label>
         <p id="js-validation-msg" class="error-message"></p>
         <button type="reset">start over</button>
 		<button type="submit">Submit</button>
