97e84950bb9e5a6577f46feeb80c107e5a28bfa7
liamjwelch <39309947+liamjwelch@users.noreply.github.com>
2020-11-11 22:17:43 -0500

    New animations
    updated .css and recommendationPage to reflect the new functionality in the recommendationBar.js

--- recommendationPage.php
+++ recommendationPage.php
@@ -10,18 +10,29 @@ else {
 }
 $content = "
 
-<script src='js/recommendationBar.js' type='text/javascript'></script> 
-  
+<script src='recommendationBar.js'></script> 
+
 <div id='myProgress'>
-  <h1>Your journey to <i>Higher</i> Education begins here...</h1>
-  <h5>Click the button below to initiate a search using our AI driven 
-    <b><i>Match Me&#169;</i></b> algorithm. What are you waiting for? Your future awaits.</h5>
+  <div id='congratulations'><h1>CONGRATULATIONS...</h1></div>
+  <div id='congratulationsII'><h2>you matched with...</h2></div>
+
+  <div id='afterBarCompletion'>
+  <h2>Click the button below to initiate a search using our AI driven 
+    <b><i>Match Me&#169;</i></b> algorithm.</h2> <h4>What are you waiting for? 
+      <br>
+      <br>Your future awaits...</h4>
+  <button id='matchButton' onclick='move();''>Match Me!</button>
+  <br>
+  <br>
+  <div id='progressBar'>
   <div id='myBarOutline'><div id='myBar'>0%</div></div>
   <div><b><span id='infoPrintOut'></span></b></div>
   <br>
-  <button id='matchButton' onclick='move()>Match Me!</button>
-  <h1 id='congratulations'></h1>
+  </div>
 </div>
+  
+</div>
+<img src='images/Concordia-logo.jpeg' alt='Concordia Logo' id='hiddenLogo'>
 
 
 ";
