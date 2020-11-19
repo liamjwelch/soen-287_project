2fdc764451a9fa9e41b57b111745b0c4d9d76b1a
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-11-19 11:19:42 -0500

    Rename the username column to email

--- create_table.php
+++ create_table.php
@@ -23,7 +23,7 @@ else {
 
 function createTable($connection, $name) {
     global $message;
-    $statement = $connection->prepare("CREATE TABLE $name (username VARCHAR(50) PRIMARY KEY NOT NULL,
+    $statement = $connection->prepare("CREATE TABLE $name (email VARCHAR(50) PRIMARY KEY NOT NULL,
                                       password VARCHAR(255) NOT NULL, role VARCHAR(10) NOT NULL, 
                                       firstName VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL)");
     try {
