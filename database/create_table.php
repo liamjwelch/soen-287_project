8b0cce2f474562382f58c54c517e5f03d162ccb6
Nicolas Aubry <n_aubry@encs.concordia.ca>
2020-11-20 08:55:20 -0500

    Add emailVerified field to user table

--- database/create_table.php
+++ database/create_table.php
@@ -25,7 +25,8 @@ function createTable($connection, $name) {
     global $message;
     $statement = $connection->prepare("CREATE TABLE $name (email VARCHAR(50) PRIMARY KEY NOT NULL,
                                       password VARCHAR(255) NOT NULL, role VARCHAR(10) NOT NULL, 
-                                      firstName VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL)");
+                                      firstName VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL,
+                                      emailVerified BOOLEAN)");
     try {
         if ($statement->execute() === TRUE) {
             $message .= "Table users created successfully <br>";
--- database/users.php
+++ database/users.php
@@ -7,12 +7,14 @@ $table_name = "users";
 function addUser($email, $password, $role, $firstName, $lastName) {
     global $table_name;
     $connection = createConnection();
-    $statement = $connection->prepare("INSERT INTO $table_name VALUES (:email, :pass, :role, :first, :last)");
+    $statement = $connection->prepare("INSERT INTO $table_name VALUES (:email, :pass, :role, :first, :last,
+                                                                       :emailVerified)");
     $statement->bindValue("email", $email);
     $statement->bindValue("pass", $password);
     $statement->bindValue("role", $role);
     $statement->bindValue("first", $firstName);
     $statement->bindValue("last", $lastName);
+    $statement->bindValue("emailVerified", 0);
     $statement->execute();
     return $statement->rowCount() === 1;
 }
