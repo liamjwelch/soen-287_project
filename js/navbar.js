f86adc011f8b102d69efdab3029ea89c30d04fae
esolis14 <e_solis@encs.concordia.ca>
2020-11-23 02:56:50 +0100

    variable type changed

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 // Add active class to the current page
-var page = window.location.href.replace(window.location.origin + "/soen287-project/", "");
-var elements = document.getElementsByClassName('page');
-for(var i = 0; i < elements.length; i++){
-    if(elements[i].getAttribute('href') == page) {
+const page = window.location.href.replace(window.location.origin + "/soen287-project/", "");
+const elements = document.getElementsByClassName('page');
+for(let i = 0; i < elements.length; i++){
+    if(elements[i].getAttribute('href') === page) {
         elements[i].className = elements[i].getAttribute('class') + " active";
     }
 }
