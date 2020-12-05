0c5f0b8d3eee14c67398bce5199a86e577e00377
philippe <philippe.rabbat.pr@gmail.com>
2020-12-05 11:43:04 -0500

    Here are the functions for sending emails. I've included the vendor folder from the assignment

---
+++ email.php
@@ -0,0 +1,116 @@
+
+<?php
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        date_default_timezone_set('Etc/UTC');
+
+        //Email for new match
+        function sendNewMatchEmail ($name, $email, $match)
+        {
+            require 'vendor/autoload.php';
+
+            $mail = new PHPMailer;
+
+            $mail->isSMTP();
+
+            $mail->Host = 'smtp.gmail.com';
+
+            $mail->Port = 587;
+
+            $mail->SMTPAuth = true;
+
+            $mail->Username = 'higher.education.database@gmail.com';
+
+            $mail->Password = '$soen287';
+
+            $mail->setFrom('higher.education.database@gmail.com', 'N.A.H.E.D. Team');
+
+            $mail->addAddress($email, $name);
+
+            $mail->isHTML(true);
+
+            $mail->Subject = 'New Match Found!';
+        
+            $mail->Body = "Dear ".$name." ".",<br><br>You have a match with a University \"$match\"";
+        
+            if (!$mail->send()) {
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            } else {
+                echo "<br>Message was sent Successfully!";
+            }
+        }
+
+        //Email for match Deadline
+        function sendMatchDeadlineEmail ($name, $email, $match, $deadline)
+        {
+            require 'vendor/autoload.php';
+
+            $mail = new PHPMailer;
+
+            $mail->isSMTP();
+
+            $mail->Host = 'smtp.gmail.com';
+
+            $mail->Port = 587;
+
+            $mail->SMTPAuth = true;
+
+            $mail->Username = 'higher.education.database@gmail.com';
+
+            $mail->Password = '$soen287';
+
+            $mail->setFrom('higher.education.database@gmail.com', 'N.A.H.E.D. Team');
+
+            $mail->addAddress($email, $name);
+
+            $mail->isHTML(true);
+
+            $mail->Subject = 'Match Deadline Approaching!';
+        
+            $mail->Body = "Dear ".$name." ".",<br><br>The deadline for \"$match\" is $deadline";
+        
+            if (!$mail->send()) {
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            } else {
+                echo "<br>Message was sent Successfully!";
+            }
+        }
+
+        //Email for Account verification
+        function sendAccountCreationEmail ($name, $email, $link)
+        {
+            require 'vendor/autoload.php';
+
+            $mail = new PHPMailer;
+
+            $mail->isSMTP();
+
+            $mail->Host = 'smtp.gmail.com';
+
+            $mail->Port = 587;
+
+            $mail->SMTPAuth = true;
+
+            $mail->Username = 'higher.education.database@gmail.com';
+
+            $mail->Password = '$soen287';
+
+            $mail->setFrom('higher.education.database@gmail.com', 'N.A.H.E.D. Team');
+
+            $mail->addAddress($email, $name);
+
+            $mail->isHTML(true);
+
+            $mail->Subject = 'Account Created!';
+        
+            $mail->Body = "Dear ".$name." ".",<br><br>Thank you for creating an account at the North American Higher Education Database. Please click the following link to confirm your account.
+            <br><br>Click here: $link<br><br>If this was not you please contact us at higher.education.database@gmail.com";
+        
+            if (!$mail->send()) {
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            } else {
+                echo "<br>Message was sent Successfully!";
+            }
+        }
+    
+?>
