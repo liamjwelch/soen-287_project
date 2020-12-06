68a17da044ad89c82bade2a359f5404363577eed
esolis14 <e_solis@encs.concordia.ca>
2020-12-06 19:16:27 +0100

    university profile: html, css and scholarships filtering

--- university.php
+++ university.php
@@ -1,7 +1,15 @@
 <?php
 require "database/universities.php";
 session_start();
-$university = getUniversity('harvard');
+$university = getUniversity($_GET['id']);
+$programs = $university['programs'];
+$score = 0.5;
+// Not sure how we'll access this elements
+//$gpa = $_SESSION['user']['gpa'];
+$gpa = floatval(3.50);
+//$budget = $_SESSION['user']['budget'];
+$budget = 10000;
+$residence = "United States";
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +19,7 @@ $university = getUniversity('harvard');
     <title>"North America Higher Education Database"</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/university.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <?php
@@ -19,27 +28,113 @@ include_once "navbar.php";
 <article class="profile-background">
     <img src='<?= getUniversityImageCompleteFilename($university['id']); ?>' alt='<?= $university['id']; ?>' class='uni-image'>
 </article>
+<section class="profile-image">
+    <img src="<?= getUniversityLogoCompleteFilename($university['id']); ?>" alt="logo" id="uniLogo">
+</section>
 <article class="wrapper">
     <aside class="contact-info">
-        <section class="profile-image">
-            <img src="<?= getUniversityLogoCompleteFilename($university['id']); ?>" alt="logo" id="uniLogo">
-        </section>
         <section class="info">
             <h4>CONTACT INFORMATION</h4>
-            <p class="info-title">Tlf:</p>
-            <p><?= $university['phone']; ?></p>
-            <p class="info-title">Email:</p>
-            <p><?= $university['email']; ?></p>
-            <p class="info-title">Webpage:</p>
-            <p><a href="<?= $university['contactPage']; ?>"><?= $university['contactPage']; ?></a></p>
-            <p class="info-title">Address:</p>
-            <p><?= $university['address']; ?></p>
+            <p class="info-title"><strong>Tlf: </strong><?= $university['phone']; ?></p>
+            <p class="info-title"><strong>Email: </strong><?= $university['email']; ?></p>
+            <p class="info-title"><strong>Webpage: </strong><a href="<?= $university['contactPage']; ?>"><?= $university['contactPage']; ?></a></p>
+            <p class="info-title"><strong>Address:</strong><br><?= $university['address']; ?></p>
         </section>
     </aside>
     <article class="profile-main">
-
+        <section id="title">
+            <h1><?= $university['name']; ?>
+                <button type="submit" name="sendEmail" value="sendEmail"><i class="fa fa-envelope"></i> Send email</button>
+            </h1>
+            <p>World Ranking: <?= $university['ranking']; ?></p>
+            <h4>Matching score: <?= $score ?></h4>
+        </section>
+        <nav class="profile-bar">
+            <a class="active" href="#description" id="navDescription"><i class="fa fa-info icons"></i><p>General Info</p></a>
+            <a href="#programs" id="navPrograms"><i class="fa fa-book icons"></i><p>Programs</p></a>
+            <a href="#application" id="navApplication"><i class="fa fa-bell icons"></i><p>Applications</p></a>
+            <a href="#financial" id="navFinancial"><i class="fa fa-money icons"></i><p>Financial Aid</p></a>
+        </nav>
+        <section class="profile-section" id="description">
+            <p><?= $university['description']; ?></p>
+        </section>
+        <section class="profile-section" id="programs">
+            <table id="programs-table">
+                <?php if(!empty($programs)) {?>
+                    <tr>
+                        <th>Program</th>
+                        <th>Minimum GPA</th>
+                    </tr>
+                <?php }?>
+                <?php foreach ($university['programs'] as $program) {?>
+                    <tr>
+                        <td><?= $program['name']; ?></td>
+                        <td><?= $program['minimumGPA']; ?></td>
+                    </tr>
+                <?php }?>
+            </table>
+        </section>
+        <section class="profile-section" id="application">
+            <section id="deadline">
+                <a href="<?= $university['contactPage']; ?>">
+                    <table>
+                    <tr>
+                        <td>
+                            <img src="images/notification.gif" alt="Notification">
+                        </td>
+                        <td id="deadlineData">
+                            <p><strong>Deadline: </strong><?= $university['deadline']; ?></p>
+                            <p><strong>Average GPA: </strong><?= $university['avgGPA']; ?></p> 
+                        </td>
+                    </tr>
+                    </table>
+                </a>
+            </section>
+        </section>
+        <section class="profile-section" id="financial">
+                <?php foreach ($university['scholarships'] as $scholarship) {?>
+                    <section class="scholarship">
+                        <h4><strong>Scholarship: </strong><?= $scholarship['name']; ?></h4>
+                        <p name="gpa"><strong>Minimum GPA: </strong><?= $scholarship['minimumGPA']; ?></p>
+                        <p><strong>Amount: </strong><?= $scholarship['amount']; ?>$</p>
+                        <?php if($scholarship['financialNeed'] !== null) {?>
+                            <p name="financialNeed"><strong>Financial need: </strong><?= $scholarship['financialNeed']; ?>$</p>
+                        <?php } else {?>
+                            <p name="financialNeed"><strong>Financial need: </strong>0$</p>
+                        <?php }?>
+                    </section>
+                <?php } ?>
+                <button type="button" name="eligibility" value="Eligibility" onclick="checkEligibility()"><i class="fa fa-check"></i> Check eligibility</button>
+        </section>
     </article>
 </article>
+<script>
+    // Display scholarships depending on user's eligibility
+    function checkEligibility() {
+        const scholarships = document.getElementsByClassName('scholarship');
+        for (let i = 0; i < scholarships.length; i++) {
+            scholarships.item(i).style.display = "";
+
+            // Get the values of the scholarship
+            let gpa = scholarships.item(i).getElementsByTagName('p').namedItem('gpa').innerText.replace('Minimum GPA: ', "");
+            if(gpa !== null) {
+                gpa = gpa.replace('Minimum GPA: ', "");
+            }
+            let financialNeed = scholarships[i].getElementsByTagName('p').namedItem('financialNeed').innerText.replace('Financial need: ', "");
+            if(financialNeed !== null) {
+                financialNeed = financialNeed.replace('Financial need: ', "");
+            }
+
+            // Validate if the student meets the requirements
+            if (gpa !== null && parseFloat(gpa) < <?=$gpa; ?>) {
+                scholarships.item(i).style.display = "none";
+            } else if(financialNeed !== null && parseInt(financialNeed.slice(0, -1)) > <?=$budget; ?>) {
+                scholarships.item(i).style.display = "none";
+            }
+        }
+    }
+</script>
+<script src="js/profile.js" type="text/javascript"></script>
 <?php
 readfile("footer.html");
 ?>
