638ee63aab13369982dcf6e40e661d02351b45b6
esolis14 <e_solis@encs.concordia.ca>
2020-12-06 19:13:23 +0100

    conflicts with profile navbar fixed

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="page-bar">
     <img class="logo" src="images/logo.png" alt="Logo">
     <a href="profile.php" class="page"><i class="fa fa-fw fa-user fa-lg icons"></i> Profile</a>
     <a href="search.php" class="page"><i class="fa fa-fw fa-search fa-lg icons"></i> Search</a>
