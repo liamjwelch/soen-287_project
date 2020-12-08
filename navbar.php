8fe6d5abc6d3582925906e77af8ee8479803960d
liamjwelch <39309947+liamjwelch@users.noreply.github.com>
2020-12-07 19:38:52 -0500

    Student Profile Display Form
    Here is the necessary additions for the student profile display:
    
    Upon completion of the studentProfileCreationForm, the user is immediately redirected to this page.
    
    The navbar is linked to this page.
    
    The student profile displays information retrieved from the DB.

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <img class="logo" src="images/logo.png" alt="Logo">
-    <a href="profile.php" class="page"><i class="fa fa-fw fa-user fa-lg icons"></i> Profile</a>
+    <a href="studentProfileDisplay.php" class="page"><i class="fa fa-fw fa-user fa-lg icons"></i> Profile</a>
     <a href="search.php" class="page"><i class="fa fa-fw fa-search fa-lg icons"></i> Search</a>
     <a href="recommendations.php" class="page"><i class="fa fa-fw fa-graduation-cap fa-lg icons"></i> Recommendations</a>
     <a href="homepage.php" class="page"><i class="fa fa-fw fa-home fa-lg icons"></i> Home</a>
