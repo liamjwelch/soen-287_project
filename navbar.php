767c0bc83aacc8e61745002716886534d88fdc5c
esolis14 <e_solis@encs.concordia.ca>
2020-12-08 01:26:45 +0100

    logout moved to navbar

--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
 <nav>
     <img class="logo" src="images/logo.png" alt="Logo">
+    <a href="logout.php" class="page"><i class="fa fa-fw fa-sign-out fa-lg icons"></i> Log out</a>
     <a href="profile.php" class="page"><i class="fa fa-fw fa-user fa-lg icons"></i> Profile</a>
     <a href="search.php" class="page"><i class="fa fa-fw fa-search fa-lg icons"></i> Search</a>
     <a href="recommendations.php" class="page"><i class="fa fa-fw fa-graduation-cap fa-lg icons"></i> Recommendations</a>
