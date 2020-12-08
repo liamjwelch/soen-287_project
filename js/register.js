61fe79e0de06bc7c416b3866f8037335a4a99d8e
AlexandreEid <alexandreeid1@hotmail.com>
2020-12-07 22:24:03 -0500

    fixes

--- js/register.js
+++ js/register.js
@@ -1,6 +1,7 @@
 function validateForm() {
     if(!validatePassword()) {
-        var message = "Password has to be 10 characters minimum, and must contain one uppercase letter, one lowercase letter, a digit, and a special character";
+        var message = "Password has to be 10 characters minimum," 
+                    + "and must contain one uppercase letter, one lowercase letter, a digit, and a special character";
         var display = document.getElementById("js-validation-msg");
         document.forms[0].password.value = "";
         document.forms[0].confirm.value = "";
@@ -20,12 +21,10 @@ function validateForm() {
 }
 
 function validatePassword() {
-    console.log('here')
     var password = document.forms[0].password.value;
     var valid = false;
 
-    console.log(password, /(?=.{10,})/.test(password), password.length)
-    if(/^(?=.*[a-z])/.test(password) && /^(?=.*[A-Z])/.test(password) && /^(?=.*[0-9])/.test(password) && /^(?=.*[!@#\$%\^&\*])/.test(password) && /(?=.{10,})/.test(password)) {
+    if(/^(.*[a-z])/.test(password) && /^(.*[A-Z])/.test(password) && /^(.*[0-9])/.test(password) && /^(.*[!@#\$%\^&\*])/.test(password) && /(.{10,})/.test(password)) {
         valid = true;
     }
 
@@ -35,9 +34,5 @@ function validatePassword() {
 function confirmPassword() {
     var password = document.forms[0].password.value;
     var confirm = document.forms[0].confirm.value;
-    var match = false;
-    if(password == confirm) {
-        match = true;
-    }
-    return match;
+    return password === confirm;
 }
